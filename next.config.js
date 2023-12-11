165c63b01903bf3565ac8fb341c700d2b5fcde61
polash <34005640+sshahriazz@users.noreply.github.com>
2023-12-11 17:10:16 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   experimental: {
     largePageDataBytes: 1024 * 10000,
     serverActionsBodySizeLimit: 1024 * 10000,
